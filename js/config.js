0c42d4ef319d4f18d84ce23dea551b88232efb96
shaymonutd <shaymon.thompson@utdallas.edu>
2025-11-19 16:22:32 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_gHKOw2I9L', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1knip74rl9b5n4hlhmaa9s2j1n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4d68pi3ddqibr2r3jd123p6l2l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
