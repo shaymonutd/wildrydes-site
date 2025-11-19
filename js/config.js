b45d1b5911a1e1acef7bc931ec601a0c0d387e9a
shaymonutd <shaymon.thompson@utdallas.edu>
2025-11-19 16:55:05 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,9 @@ window._config = {
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://5wyomkpry8.execute-api.us-west-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
